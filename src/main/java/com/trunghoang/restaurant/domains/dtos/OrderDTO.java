57c7cefae963a876c3806579ed411220f9db3832
Trung Hoang <hoangminhtrung1995@gmail.com>
2019-12-08 18:09:09 +0700

    Update toString/hasCode equals

--- src/main/java/com/trunghoang/restaurant/domains/dtos/OrderDTO.java
+++ src/main/java/com/trunghoang/restaurant/domains/dtos/OrderDTO.java
@@ -9,9 +9,50 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class OrderDTO {
+public class OrderDTO implements IdDTO {
 
 	private long id;
 	private int quantity;
 	private Timestamp date;
+
+	@Override
+	public String toString() {
+		return "OrderDTO [id=" + id + ", quantity=" + quantity + ", date=" + date + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + quantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj instanceof BillDTO) {
+			OrderDTO that = (OrderDTO) obj;
+			return this.id == that.id && this.date.equals(that.date) && this.quantity == that.quantity;
+		}
+		return false;
+
+	}
+
+	public String toStrings() {
+		StringBuilder result = new StringBuilder();
+		result.append("OrderDTO [id=");
+		result.append(id);
+		result.append(", quantity=");
+		result.append(quantity);
+		result.append(", date=");
+		result.append(date);
+		result.append("]");
+		return result.toString();
+	}
+
 }
