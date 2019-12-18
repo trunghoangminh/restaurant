f0dc2559cd2682aa334faee308ad7ced8a0f8c61
Trung Hoang <hoangminhtrung1995@gmail.com>
2019-12-18 18:55:56 +0700

    Completed application

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Restaurant
+1. Build code
+- `cd app`
+- `mvn clean install`
+2. Build image
+- `docker-compose build`
+
+3. Start server
+- `docker-compose up`
---
+++ db/dbstart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+service mysql start
+
+mysql -u root -e "create user 'admin'@'localhost' identified by 'admin';"
+mysql -u root -e "create user 'admin'@'%' identified by 'admin';"
+mysql -u root -e "grant all privileges on *.* to 'admin'@'localhost' with grant option;"
+mysql -u root -e "grant all privileges on *.* to 'admin'@'%' with grant option;"
+mysql -u root -e "set password for 'admin'@'localhost' = PASSWORD('admin');"
+mysql -u root -e "set password for 'admin'@'%' = PASSWORD('admin');"
+mysql -u root -e "create database restaurant;"
+
+tail -F -n0 /etc/hosts
